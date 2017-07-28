d6f0f17ac042e560417b9d4ece4fd0cc6d82139c
matt pinner <mpinner@gmail.com>
2017-07-29 07:10:55 +0800

    added star spawning script and serial assets

--- StarCatcher2/Assets/Scripts/StarSpawn.cs
+++ StarCatcher2/Assets/Scripts/StarSpawn.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class StarSpawn : MonoBehaviour {
 
+    public GameObject prefab;
+    
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+
+    private static ArrayList starStarts;
+    private static Random random = new Random();
+
+    // empty game object as parent for spwned stars
+    private GameObject parent;
+
+    // Use this for initialization
+    void Start () {
+
+        parent = GameObject.FindGameObjectWithTag("STARS");
+
+        InvokeRepeating("Spawn", 0.5f, 5.0f);
+
+        starStarts = new ArrayList();
+        for (int i=0; i<5; i++)
+        {
+            starStarts.Add(new Vector3(i, i * 0.2f, 10));
+        }
+
+        
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public static void spawn()
+    public void Spawn()
     {
+
+        int startPosition = Random.Range(0, starStarts.Count);
+        Vector3 start = (Vector3)starStarts.ToArray().GetValue(startPosition);
+
+
+        GameObject star = Instantiate(prefab, start, Quaternion.identity) as GameObject;
+        star.transform.parent = parent.transform;
+        star.name = "star-" + startPosition;
 
 
     }
