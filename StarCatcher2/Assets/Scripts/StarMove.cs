d2f2a3474800447e3d198a4e5187a30538bf3c63
adellelin <adelle.lin@gmail.com>
2017-07-30 20:44:59 -0700

    created strip class with spawn location and strip length in StarMove, new way of calculating speed, startMarker and nearlyEndMarker

--- StarCatcher2/Assets/Scripts/StarMove.cs
+++ StarCatcher2/Assets/Scripts/StarMove.cs
@@ -12,22 +12,26 @@ public class StarMove : MonoBehaviour {
     private float journeyLength;
     bool timeRecorded = false;
     float fallDuration = 0f;
+    float stripLength = 0.5f;
+    public Vector3 starDropScale =  new Vector3(1, 3.0f, 1);
+    public float timeToDestroyStar = 2f;
     
     void Start()
     {
         // turn off trigger component so players can't catch stars at the start
         gameObject.GetComponent<SphereCollider>().isTrigger = false;
         startTime = Time.time;
- 
-        startMarker = transform.localPosition;
-        endMarker = startMarker + new Vector3(0, -3.0f, 0);
+
+        endMarker = transform.localPosition;
+        startMarker = endMarker + stripLength * starDropScale;
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+        speed = journeyLength / fallDuration;
 
         // set time when the trigger component is turned on
         //nearlyEndMarker = startMarker + new Vector3(0, 0.9f * endMarker[1], 0);
-        nearlyEndMarker = startMarker + new Vector3(0, -2.5f, 0);
-        journeyLength = Vector3.Distance(startMarker, endMarker);
+        nearlyEndMarker = endMarker + new Vector3(0, +0.1f, 0);
         // set speed of fall according to fall time and distance travelled
-        speed = journeyLength / fallDuration;
+
 
     }
 
@@ -42,19 +46,21 @@ public class StarMove : MonoBehaviour {
         {
             speed += .1f/2;
         }
-        */
+     
+        //when speed is variable, use this to calculate fall duration
         else if (transform.position == endMarker && !timeRecorded)
         {
             fallDuration = Time.time - startTime;
             Debug.Log("time the star took to fall " + fallDuration);
             timeRecorded = true;
         }
-         if (transform.position[1] <= nearlyEndMarker[1])
+           */
+        if (transform.position[1] <= nearlyEndMarker[1])
         {
             gameObject.GetComponent<SphereCollider>().isTrigger = true;
             
         }
-        Destroy(gameObject, fallDuration + 5.0f);
+        Destroy(gameObject, fallDuration + timeToDestroyStar);
         //Debug.Log(gameObject.GetComponent<SphereCollider>().isTrigger);
 
 
