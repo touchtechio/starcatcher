5e4a369281f6ae59a6664e3bae4f07917944840b
adelle lin <adelle.lin@gmail.com>
2017-08-07 11:44:18 +0800

    Removed logging from strip position Scripts/StripPosition.cs Also removed infinite loop check for star distances in starspawn (most probably the problem for the game hanging after setting star positions, as I was testing placing stars in same place)

---
+++ StarCatcherUnity/Assets/Scripts/stripPositionLog.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class stripPositionLog : MonoBehaviour {
+    public GameObject stripCountObject;
+    public Text stripCountText;
+    public bool clearStripArray = false;
+    private Scene currentScene;
+    int starStripLogger;
+    public StripPosition stripPosition;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        stripPosition = FindObjectOfType<StripPosition>();
+        starStripLogger = stripPosition.starStripCount;
+        //Debug.Log(starStripLogger);
+
+
+        stripCountObject = GameObject.Find("StripLog");
+        stripCountText = stripCountObject.GetComponent<Text>();
+
+        // clear the array of strip positions to repopulate;
+        if (clearStripArray == true)
+        {
+
+            stripCountText.text = "Clear";
+            clearStripArray = false;
+        }
+
+    }
+
+    public void LogStrips()
+    {
+        stripCountText.text = (starStripLogger).ToString();
+    }
+}
