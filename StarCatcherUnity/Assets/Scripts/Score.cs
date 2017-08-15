87e1e59138ed8a97fce06d006765f061a0713a71
adellelin <adelle.lin@gmail.com>
2017-08-14 19:42:18 -0700

    adding level interface

--- StarCatcherUnity/Assets/Scripts/Score.cs
+++ StarCatcherUnity/Assets/Scripts/Score.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,12 @@ public class Score : MonoBehaviour {
 
     public static int score;
     private static Constellations constellations;
+    internal static readonly object LEVEL_ONE;
+    internal static readonly object LEVEL_TWO;
+    internal static readonly object LEVEL_THREE;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         score = 0;
         constellations = FindObjectOfType<Constellations>();
 
@@ -30,5 +34,9 @@ public class Score : MonoBehaviour {
 //        Debug.Log("caught star " + score);
         return constellations.GetNextEmptyPosition().transform.position;
     }
-    
+
+    internal static object GetCurrentLevel()
+    {
+        throw new NotImplementedException();
+    }
 }
