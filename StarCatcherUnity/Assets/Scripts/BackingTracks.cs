0bab2e68f1a5c04081e46a9f8f6e5d50c043f139
matt pinner <mpinner@gmail.com>
2017-08-15 11:28:49 +0800

    rototates backing track with new level (first pass simple level changes)

---
+++ StarCatcherUnity/Assets/Scripts/BackingTracks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackingTracks : MonoBehaviour {
+
+    public AudioClip[] levelAudio;
+    int i = 0;
+
+	public void UpdateLevel () {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = GetAudioClipForLevel();
+        audio.Play();
+    }
+
+    AudioClip GetAudioClipForLevel()
+    {
+        return levelAudio[(i++ % levelAudio.Length)];
+    }
+}
