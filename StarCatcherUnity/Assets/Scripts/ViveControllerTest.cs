89489811f6390bec3ceafd69b9c245345cf5c0d0
adellelin <adelle.lin@gmail.com>
2022-05-20 01:28:47 +0100

    cleaning up vive test scripts
    cleaning up vive test scripts

--- StarCatcherUnity/Assets/Scripts/ViveControllerTest.cs
+++ StarCatcherUnity/Assets/Scripts/ViveControllerTest.cs
@@ -12,14 +12,8 @@ public class ViveControllerTest : MonoBehaviour
     //Hand to get (right hand or left hand or any hand)
     public SteamVR_Input_Sources handType;
     public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
-    public SteamVR_Action_Vector2 AnalogStickLeft;
     public SteamVR_Action_Boolean SphereOnOff;
     public SteamVR_Action_Pose poseAction = null;
-    public GameObject leftController;
-    //SteamVR_Action_Pose poseActionL;
-    //poseActionL = SteamVR_Input.GetAction<SteamVR_Action_Pose>("Pose_left_tip");
-    //vPosition = poseActionR[SteamVR_Input_Sources.LeftHand].localPosition;
-    //qRotation = poseActionR[SteamVR_Input_Sources.LeftHand].localRotation;
 
     //reference to the sphere
     public GameObject Sphere;
@@ -39,12 +33,7 @@ public class ViveControllerTest : MonoBehaviour
 
     void Update()
     {
-        //Debug.Log("position"+leftController.transform.position.x);
         triggerPosition = butterflyNet.transform.position;
-        //Vector2 stickVector = AnalogStickLeft.GetAxis(handType);
-        //player.transform.position += speed * new Vector3(stickVector.x, 0, stickVector.y);
-        //Debug.Log("stickVector: " + stickVector.x + ": " + stickVector.y);
-        //Debug.Log("tringger: " + SphereOnOff.GetState(handType));
     }
 
     public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
