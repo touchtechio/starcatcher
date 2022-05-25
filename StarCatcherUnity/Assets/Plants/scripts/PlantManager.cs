c0d61a73be1fc45d919c62e277445c1b1a2d734b
Andy Wallace <andymasteroffish@gmail.com>
2022-05-25 17:08:30 -0400

    plants use cumulativeEnvironmentDamageScore + a lerping value

--- StarCatcherUnity/Assets/Plants/scripts/PlantManager.cs
+++ StarCatcherUnity/Assets/Plants/scripts/PlantManager.cs
@@ -9,16 +9,24 @@ Manager class that runs the plants
 
 public class PlantManager : MonoBehaviour
 {
+    public static PlantManager instance = null;
+
     public string[] root_ids;
 
     private List<PlantRoot> roots = new List<PlantRoot>();
 
-    public int max_depth;
+    public int max_depth;   //this should be plant by plant
 
+    //global health
+    private float cur_health;
+    public float health_lerp;
+
+    //debug tools
+    [Header("Debug Tools")]
     public bool use_debug_sprite_color;
 
 
-    public static PlantManager instance = null;
+    
 
     void Awake(){
         if (instance == null){
@@ -45,19 +53,26 @@ public class PlantManager : MonoBehaviour
             string root_id = root_ids[ (int)Random.Range(0,root_ids.Length)];
             roots.Add( new PlantRoot(root_id, pos));
         }
+
+        cur_health = 1.0f - Score.cumulativeEnvironmentDamageScore;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug spacebar to reset
         if (Input.GetKeyDown(KeyCode.Space)){
             reset();
         }
 
-        float health_value = 0.9f;//health_slider.value;
+        //grab the health value from the game
+        //This value is treated as damage, so it is inverted (1=dead, 0=alive)
+        float raw_health_value = Mathf.Clamp(1.0f-Score.cumulativeEnvironmentDamageScore, 0.0f, 1.0f);
+        //lerp it
+        cur_health = Mathf.Lerp(cur_health, raw_health_value, health_lerp);
 
         for (int i=0; i<roots.Count; i++){
-            roots[i].set_health( health_value );
+            roots[i].set_health( cur_health );
         }
     }
 }
