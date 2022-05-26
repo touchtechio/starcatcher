30d5ed44f1593bc613b387f619e3ec2f9d3f9ba0
Andy Wallace <andymasteroffish@gmail.com>
2022-05-26 17:30:00 -0400

    added the plants

---
+++ StarCatcherUnity/Assets/Plants/scripts/PlantKickoff.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This is a nothing script that just instantiates the plant amanger prefab
+
+I mostly made this so that I can work on the plants without changing anything in the scene
+*/
+
+public class PlantKickoff : MonoBehaviour
+{
+
+    public GameObject plant_manager_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(plant_manager_prefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StarCatcherUnity/Assets/Plants/scripts/PlantPartPool.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+This is a singleton class dedicated to grabbing plant limb prefabs
+
+I cannot instantiate the limbs directly form inside their prefab because the recursive nature causes the original prefab to get the children
+I'm not sure exactly why this is, but setting up a pooler like this seems to solve it
+
+*/
+
+public class PlantPartPool : MonoBehaviour
+{
+    [System.Serializable]
+   public struct LimbObj { 
+       public string id_name; 
+       public GameObject prefab; 
+       //public int max_depth_if_trunk;
+    }
+
+   public LimbObj[] limb_objs;
+
+
+   public static PlantPartPool instance = null;
+
+    void Awake(){
+        if (instance == null){
+            instance = this;
+        }else if (instance != this) {
+            Destroy (gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject get_limb(string limb_id){
+        for (int i=0; i<limb_objs.Length; i++){
+            if (limb_objs[i].id_name == limb_id){
+                return limb_objs[i].prefab;
+            }
+        }
+        Debug.Log("COULD NOT FIND:"+limb_id);
+        return null;
+    }
+}
