eaeb3e1ea76f50f03d2254e0379e11aa7f24cb4f
adellelin <adelle.lin@gmail.com>
2022-05-31 23:42:39 +0100

    Added states to spawner
    Added world states to star spawner
    Fixed score bug
    Added logging to plants screen

---
+++ StarCatcherUnity/Assets/Scripts/scoreLog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class scoreLog : MonoBehaviour {
+    public GameObject scoreObjectUI;
+    public Text scoreText;
+    private float starScoreLogger;
+
+    public GameObject worldStateUI;
+    public Text worldStateText;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        starScoreLogger = Score.cumulativeEnvironmentDamageScore;
+        //Debug.Log(starStripLogger);
+
+        scoreObjectUI = GameObject.Find("ScoreText");
+        scoreText = scoreObjectUI.GetComponent<Text>();
+        worldStateUI = GameObject.Find("WorldState");
+        worldStateText = worldStateUI.GetComponent<Text>();
+
+    }
+
+    public void LogScore()
+    {
+        scoreText.text = (starScoreLogger).ToString();
+        worldStateText.text = (Score.plasmaWorldState).ToString();
+    }
+}
